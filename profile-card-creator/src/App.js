85182072982d4abc661e7ac24cce54644f866df8
Mohammad Mahdi Paydar Puya <78438593+mahdipuya@users.noreply.github.com>
2021-08-03 09:20:58 -0700

    Create and control Details component

--- profile-card-creator/src/App.js
+++ profile-card-creator/src/App.js
@@ -1,9 +1,49 @@
+import React, { Component } from 'react'
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
+class Details extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    emailAddress: ''
+  }
+  handleChange = (e) => {
+    const { value, id } = e.target
+    this.setState( {[id]: value} )
+  }
+
+  render() {
+    return (
+      <div className="col-4 p-3">
+        <h3>Details</h3>
+        <div className="mb-3">
+          <label htmlFor="firstName" className="form-label">First Name <span className="text-danger"><b>*</b></span></label>
+          <input type="text" className="form-control" id="firstName" value={this.state.firstName} placeholder="John" onChange={this.handleChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lastName" className="form-label">Last Name <span className="text-danger"><b>*</b></span></label>
+          <input type="text" className="form-control" id="lastName" value={this.state.lastName} placeholder="Doe" onChange={this.handleChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
+          <input type="text" className="form-control" id="phoneNumber" value={this.state.phoneNumber} placeholder="+000000000000" onChange={this.handleChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="emailAddress" className="form-label">Email Address</label>
+          <input type="email" className="form-control" id="emailAddress" value={this.state.emailAddress} placeholder="johndoe@example.com" onChange={this.handleChange} />
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <Details />
     </div>
   );
 }
